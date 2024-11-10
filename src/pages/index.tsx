d6ff76c96e26b55f5c7d82bce80f471f2bef3e29
MrCooksIT <acoetzee@maristsj.co.za>
2024-11-10 12:22:03 +0200

    Update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import QuizSection from '@/components/QuizSection';
+import KarelPractical from './karel-practical'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-200 p-8">
       <QuizSection />
+      <KarelPractical />
     </div>
   );
 }
