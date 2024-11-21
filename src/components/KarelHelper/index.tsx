761f60a022f16641f12f3e3f5489f2cb8ae4e17c
MrCooksIT <acoetzee@maristsj.co.za>
2024-11-21 15:18:52 +0200

    LINKY

--- src/components/KarelHelper/index.tsx
+++ src/components/KarelHelper/index.tsx
@@ -13,7 +13,7 @@ const KarelHelper = () => {
     setTimeout(() => setCopiedSnippet(null), 1000)
   }
   const openKarelSandbox = () => {
-    window.open('https://codehs.com/sandbox/id/javascript-karel-gN9q86', '_blank')
+    window.open('https://codehs.com/sandbox/id/javascript-superkarel-ANOr1X', '_blank')
   }
   const codeSnippets = {
     basic: [
